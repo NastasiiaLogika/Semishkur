7f249948bd439e111fa3a893773db36aa050ef90
AnastasiiaKochurenkovaPM <anastasiia.kochurenkova.pm.2020@lpnu.ua>
2024-05-20 22:23:00 +0300

    weather

--- main.py
+++ main.py
@@ -24,28 +24,25 @@ class ApplicationWeather(QMainWindow):
 
         self.city_combo = QComboBox(self)
         self.city_combo.addItems(["Львів", "Київ", "Одеса", "Харків", "Ужгород", "Миколаїв", "Вінниця", "Житомир", "Тернопіль"])
+
         self.layout.addWidget(self.city_combo)
-        # self.city_combo.currentIndexChanged.connect(self.get_weather())
-
-        # self.city_combo.currentIndexChanged.connect(self.get_wather)
+        self.city_combo.currentIndexChanged.connect(self.get_weather)
         self.city_combo.setStyleSheet("""
-            QComboBox {
-                background-color: #fff;
-                border: 1px solid #ccc
-                padding: 5px;
-                border-radius: 3px;
-            }
-            QComboBox::drop-down {
-                border: none;
-            }
-
-
-        """)
+                    QComboBox {
+                        background-color: #fff;
+                        border: 1px solid #ccc;
+                        padding: 5px;
+                        border-radius: 3px;
+                    }
+                    QComboBox::drop-down {
+                        border: none;
+                    }
+                """)
         self.button = QPushButton('Отримати погоду', self)
         self.layout.addWidget(self.button)
         self.button.clicked.connect(self.get_weather)
         self.button.setStyleSheet("""
-            QpushButton {
+            QPushButton {
                 background-color: #007bff;
                 color: #fff;
                 padding: 7px 10px;
@@ -57,6 +54,34 @@ class ApplicationWeather(QMainWindow):
 
         
         """)
+
+        self.update_button = QPushButton('Оновити', self)
+        self.layout.addWidget(self.update_button)
+        self.update_button.clicked.connect(self.update_weather)
+        self.update_button.setStyleSheet("""
+                    QPushButton {
+                        background-color: #3a802d;
+                        color: #fff;
+                        padding: 7px 10px;
+                        border: none;
+                        border-radius: 3px;
+                    }
+                    QPushButton:hover {
+                        background-color: #218838;
+                    }
+                """)
+
+        self.weather_label = QLabel('', self)
+        self.layout.addWidget(self.weather_label)
+        self.weather_label.setStyleSheet("font-size: 16px;")
+
+        self.icon_label = QLabel(self)
+        self.layout.addWidget(self.icon_label)
+
+        self.last_update_label = QLabel('', self)
+        self.layout.addWidget(self.last_update_label)
+
+        self.get_weather()
 
     def get_weather(self):
         api_key = 'f81703c1f3b81ad93e6644153c4a426e'
@@ -70,25 +95,28 @@ class ApplicationWeather(QMainWindow):
             weather = data['weather'][0]['description']
             temperature = data['main']['temp']
             humidity = data['main']['humidity']
-            wind_speed = data['main']['speed']
+            wind_speed = data['wind']['speed']
             icon_name = data['weather'][0]['icon']
+            icon_url = f"http://openweathermap.org/img/w/{icon_name}.png"
 
-            weather_text = f'Погода: {weather}\nТемпература: {temperature}℃\nВологість: {humidity}%\nШвидкість вітру:{wind_speed} м/с'
+            weather_text = f'Погода: {weather}\nТемпература: {temperature}°C\nВологість: {humidity}%\nШвидкість вітру: {wind_speed} м/с'
             self.weather_label.setText(weather_text)
 
             pixmap = QPixmap()
-            pixmap.loadFromData(requests.get(icon_name).content)
+            pixmap.loadFromData(requests.get(icon_url).content)
             self.icon_label.setPixmap(pixmap)
 
             now = datetime.now()
-            formated_date = now.strftime("%d.%m.%Y %H:%M:%S")
-            self.icon_label.setText(f"Останнє оновлення: (formatted_date) ")
+            formatted_date = now.strftime("%d.%m.%Y %H:%M:%S")
+            self.last_update_label.setText(f"Останнє оновлення: {formatted_date}")
         else:
-            self.wheather_label.setText('Місто не знайдено.Спробуйте ще раз.')
+            self.weather_label.setText('Місто не знайдено.Спробуйте ще раз.')
+    def update_weather(self):
+        self.get_weather()
 
 
-
-app = QApplication(sys.argv)
-window = ApplicationWeather()
-window.show()
-sys.exit(app.exec_())
+if __name__=='__main__':
+    app = QApplication(sys.argv)
+    window = ApplicationWeather()
+    window.show()
+    sys.exit(app.exec_())
